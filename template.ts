f80bd9dd1b41b64bf668868be024846bde4080c3
Konrad Klawonn <k.klawonn@mailbox.org>
2025-10-29 11:43:24 +0100

    Expand template

--- template.ts
+++ template.ts
@@ -1,3 +1,17 @@
-import { readPuzzleInput } from "./util.ts";
+import { readPuzzleInput } from "../util.ts";
 
-const puzzle = readPuzzleInput("00");
+const puzzle = readPuzzleInput("01");
+const example = [];
+
+const solve1 = (input: string[]): any => {
+  return undefined;
+};
+
+const solve2 = (input: string[]): any => {
+  return undefined;
+};
+
+console.log(`Part 1 example: ${solve1(example)}`);
+console.log(`Part 1 puzzle : ${solve1(puzzle)}`);
+console.log(`Part 2 example: ${solve2(example)}`);
+console.log(`Part 2 puzzle : ${solve2(puzzle)}`);
