4966a8192bf26b47db41b9ad92f9b5ba7d7ac14f
Konrad Klawonn <k.klawonn@mailbox.org>
2025-12-03 08:23:53 +0100

    Solve Day 03

--- solutions/03.ts
+++ solutions/03.ts
@@ -17,8 +17,51 @@ const findMaxJoltage = (bank: number[]): number => {
       b = 0;
     } else if (battery > b) b = battery;
   }
-  console.log(`${bank} - ${a}${b}`);
   return a * 10 + b;
+};
+
+// I don't feel like being clever today...
+const findMaxJoltage12 = (bank: number[]): number => {
+  let batteries = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (let [i, battery] of bank.entries()) {
+    if (battery > batteries[0]! && i + 11 < bank.length) {
+      batteries[0] = battery;
+      batteries.fill(0, 1);
+    } else if (battery > batteries[1]! && i + 10 < bank.length) {
+      batteries[1] = battery;
+      batteries.fill(0, 2);
+    } else if (battery > batteries[2]! && i + 9 < bank.length) {
+      batteries[2] = battery;
+      batteries.fill(0, 3);
+    } else if (battery > batteries[3]! && i + 8 < bank.length) {
+      batteries[3] = battery;
+      batteries.fill(0, 4);
+    } else if (battery > batteries[4]! && i + 7 < bank.length) {
+      batteries[4] = battery;
+      batteries.fill(0, 5);
+    } else if (battery > batteries[5]! && i + 6 < bank.length) {
+      batteries[5] = battery;
+      batteries.fill(0, 6);
+    } else if (battery > batteries[6]! && i + 5 < bank.length) {
+      batteries[6] = battery;
+      batteries.fill(0, 7);
+    } else if (battery > batteries[7]! && i + 4 < bank.length) {
+      batteries[7] = battery;
+      batteries.fill(0, 8);
+    } else if (battery > batteries[8]! && i + 3 < bank.length) {
+      batteries[8] = battery;
+      batteries.fill(0, 9);
+    } else if (battery > batteries[9]! && i + 2 < bank.length) {
+      batteries[9] = battery;
+      batteries.fill(0, 10);
+    } else if (battery > batteries[10]! && i + 1 < bank.length) {
+      batteries[10] = battery;
+      batteries.fill(0, 11);
+    } else if (battery > batteries[11]!) {
+      batteries[11] = battery;
+    }
+  }
+  return Number(batteries.map(String).join(""));
 };
 
 const solve1 = (input: string[]): number => {
@@ -28,10 +71,12 @@ const solve1 = (input: string[]): number => {
 };
 
 const solve2 = (input: string[]): number => {
-  return 0;
+  return input
+    .map((bank) => findMaxJoltage12(bank.split("").map(Number)))
+    .reduce((a, b) => a + b);
 };
 
 assert(solve1(example) === 357);
 console.log(`Part 1 : ${solve1(puzzle)}`);
-assert(solve2(example) === 0);
+assert(solve2(example) === 3121910778619);
 console.log(`Part 2 : ${solve2(puzzle)}`);
