64e02725f85cf7054c63aa3fb1b117bbff9a54e6
Konrad Klawonn <k.klawonn@mailbox.org>
2025-12-09 20:05:24 +0100

    Works for almost all examples

--- solutions/09.ts
+++ solutions/09.ts
@@ -16,34 +16,11 @@ type Line = {
   a: Point;
   b: Point;
 };
-type Rectangle = Line;
-
-/** A friendly line always goes from left to right or from top to bottom */
-const createFriendlyLine = (p1: Point, p2: Point): Line => {
-  if (p1.x === p2.x) {
-    // horizontal
-    if (p1.y < p2.y) return { a: p1, b: p2 };
-    else return { a: p2, b: p1 };
-  } else {
-    //vergical
-    if (p1.x < p2.x) return { a: p1, b: p2 };
-    else return { a: p2, b: p1 };
-  }
-};
-/** A friendly Rectangle always goes from top left to bottom right */
-const createFriendlyRectangle = (p1: Point, p2: Point): Line => {
-  if (p1.x <= p2.x && p1.y <= p2.y) {
-    // friendly
-    return { a: p1, b: p2 };
-  } else {
-    // nasty
-    return { a: p2, b: p1 };
-  }
-};
+type Rectangle = Line; //somehow...
 
 const parse = (input: string[]): PuzzleInput => {
   const rectangles: Rectangle[] = [];
-  let [x1, x2, y1, y2] = [
+  let [x1, y1, x2, y2] = [
     ...input[0]!.split(",").map(Number),
     ...input[input.length - 1]!.split(",").map(Number),
   ];
@@ -57,24 +34,58 @@ const parse = (input: string[]): PuzzleInput => {
     p1 = { x: x1!, y: y1! };
     [x2, y2] = input[i + 1]!.split(",").map(Number);
     p2 = { x: x2!, y: y2! };
-    lines.push(createFriendlyLine(p1, p2));
+    lines.push({ a: p1, b: p2 });
 
     for (let j = i + 1; j < input.length; j++) {
       if (!input[j]) continue;
       [x2, y2] = input[j]!.split(",").map(Number);
       p2 = { x: x2!, y: y2! };
-      rectangles.push(createFriendlyRectangle(p1, p2));
+      rectangles.push({ a: p1, b: p2 });
     }
   }
   return { rectangles: rectangles, lines: lines };
 };
 
-const calcAreaOfFriendlyRectangle = (r: Rectangle): number => {
-  return (1 + r.b.x - r.a.x) * (1 + r.b.y - r.a.y);
+const calcAreaOfRectangle = (r: Rectangle): number => {
+  return (1 + Math.abs(r.b.x - r.a.x)) * (1 + Math.abs(r.b.y - r.a.y));
+};
+
+const minX = (s: Rectangle | Line): number => {
+  return Math.min(s.a.x, s.b.x);
+};
+const maxX = (s: Rectangle | Line): number => {
+  return Math.max(s.a.x, s.b.x);
+};
+const minY = (s: Rectangle | Line): number => {
+  return Math.min(s.a.y, s.b.y);
+};
+const maxY = (s: Rectangle | Line): number => {
+  return Math.max(s.a.y, s.b.y);
+};
+
+const lineOutsideOfRect = (line: Line, rect: Rectangle): boolean => {
+  let result =
+    maxX(line) <= minX(rect) ||
+    minX(line) >= maxX(rect) ||
+    maxY(line) <= minY(rect) ||
+    minY(line) >= maxY(rect);
+
+  return result;
 };
 
 const isValidRectangle = (rectangle: Rectangle, lines: Line[]): boolean => {
-  return false;
+  for (let line of lines) {
+    if (!lineOutsideOfRect(line, rectangle)) {
+      //      console.log(
+      //        `FAIL:    rect [(${rectangle.a.x},${rectangle.a.y}),(${rectangle.b.x},${rectangle.b.y})] => line [(${line.a.x},${line.a.y}),(${line.b.x},${line.b.y})]`,
+      //      );
+      return false;
+    }
+  }
+  console.log(
+    `SUCCESS: rect [(${rectangle.a.x},${rectangle.a.y}),(${rectangle.b.x},${rectangle.b.y})] has ${calcAreaOfRectangle(rectangle)} `,
+  );
+  return true;
 };
 
 const solve = (
@@ -85,12 +96,13 @@ const solve = (
   let maxArea = 0;
   for (let rectangle of rectangles) {
     if (isValid(rectangle, lines)) {
-      let area = calcAreaOfFriendlyRectangle(rectangle);
+      let area = calcAreaOfRectangle(rectangle);
       if (area > maxArea) maxArea = area;
     }
   }
   return maxArea;
 };
+
 const solve1 = (input: string[]): number => {
   return solve(input, () => true);
 };
@@ -102,4 +114,60 @@ const solve2 = (input: string[]): number => {
 assert(solve1(example) === 50);
 console.log(`Part 1 : ${solve1(puzzle)}`);
 assert(solve2(example) === 24);
-console.log(`Part 2 : ${solve2(puzzle)}`);
+const example2 = ["4,2", "13,2", "13,4", "8,4", "8,6", "11,6", "11,10", "4,10"];
+assert(solve2(example2) === 40);
+console.log("example3");
+const example3 = [
+  "3,2",
+  "13,2",
+  "13,4",
+  "8,4",
+  "8,6",
+  "11,6",
+  "11,11",
+  "7,11",
+  "7,8",
+  "5,8",
+  "5,10",
+  "3,10",
+];
+assert(solve2(example3) === 35);
+console.log("example4");
+const example4 = [
+  "3,2",
+  "17,2",
+  "17,13",
+  "13,13",
+  "13,11",
+  "15,11",
+  "15,8",
+  "11,8",
+  "11,15",
+  "18,15",
+  "18,17",
+  "4,17",
+  "4,12",
+  "6,12",
+  "6,5",
+  "3,5",
+];
+assert(solve2(example4) === 66);
+console.log("example5");
+const example5 = [
+  "1,0",
+  "3,0",
+  "3,6",
+  "16,6",
+  "16,0",
+  "18,0",
+  "18,9",
+  "13,9",
+  "13,7",
+  "6,7",
+  "6,9",
+  "1,9",
+];
+
+assert(solve2(example5) === 30);
+console.log("###");
+console.log(`Part 2 : ${solve2(puzzle)}`); //250496 too low
