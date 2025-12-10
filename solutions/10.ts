715cdaf553bc7d7d0a114e5d46458813d9a08398
Konrad Klawonn <k.klawonn@mailbox.org>
2025-12-10 08:51:28 +0100

    Input Parser

--- solutions/10.ts
+++ solutions/10.ts
@@ -8,7 +8,32 @@ const example = [
   "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}",
 ];
 
+type PuzzleInput = {
+  target: number[];
+  switchtes: number[][];
+  joltage: number[];
+};
+
+function parse(machine: string): PuzzleInput {
+  const targetPattern = /(?<=\[)[.#]+/;
+  const switchPattern = /(?<=\]\s).+(?=\s{)/;
+  const joltagePattern = /(?<={).+(?=})/;
+  const target = machine
+    .match(targetPattern)![0]
+    .split("")
+    .map((s) => (s === "#" ? 1 : 0));
+  const switchtes = machine
+    .match(switchPattern)![0]
+    .replaceAll(/[\(\)]/g, "")
+    .split(" ")
+    .map((s) => s.split(",").map(Number));
+  const joltage = machine.match(joltagePattern)![0].split(",").map(Number);
+  return { target: target, switchtes: switchtes, joltage: joltage };
+}
+
 function findShortestSequence(machine: string): number {
+  const { target, switchtes } = parse(machine);
+  // TODO: bfs through the switches
   return 0;
 }
 
