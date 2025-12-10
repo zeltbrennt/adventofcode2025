52fb126c1710e704a4dcdb85d72ddef0b8816c13
Konrad Klawonn <k.klawonn@mailbox.org>
2025-12-10 07:55:59 +0100

    Change template to use function keyword

--- template.ts
+++ template.ts
@@ -4,13 +4,13 @@ import { readPuzzleInput } from "../util.ts";
 const puzzle = readPuzzleInput("00");
 const example = [];
 
-const solve1 = (input: string[]): number => {
+function solve1(input: string[]): number {
   return 0;
-};
+}
 
-const solve2 = (input: string[]): number => {
+function solve2(input: string[]): number {
   return 0;
-};
+}
 
 assert(solve1(example) === 0);
 console.log(`Part 1 : ${solve1(puzzle)}`);
